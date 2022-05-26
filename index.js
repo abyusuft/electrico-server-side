223fbf5980a18c202075a8a62454164dbc7735d9
abyusuft <imabuyusuf@gmail.com>
2022-05-27 02:14:41 +0600

    send user to database

--- index.js
+++ index.js
@@ -18,16 +18,58 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kqnws.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authorization.split(' ')[1];
+    console.log(token);
 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden Access" })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
         await client.connect();
 
-        //Database Collections 
+        //User database collection and API
+        const usersCollection = client.db("electrico").collection("users");
+
+        // Generate Token on UserLogin 
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const option = { upsert: true };
+            const doc = {
+                $set: user,
+            }
+            const result = await usersCollection.updateOne(filter, doc, option);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ result, token });
+        })
+
+
+
+
+        //Product database collection and API
         const productsCollection = client.db("electrico").collection("products");
+
+
+        //Review database collection and API
         const reviewCollection = client.db("electrico").collection("review");
+
+
+        //Purchase database collection and API
         const purchaseCollection = client.db("electrico").collection("purchase");
+
 
     }
     finally {
