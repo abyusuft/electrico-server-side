dde9d81925378b25eae20d3e6adb0266afa2c386
abyusuft <imabuyusuf@gmail.com>
2022-05-27 21:17:07 +0600

    Get Purchase item info api added

--- index.js
+++ index.js
@@ -119,6 +119,12 @@ async function run() {
             const result = await productsCollection.deleteOne(query);
             res.send(result);
         })
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.findOne(query);
+            res.send(result);
+        })
 
 
         //Review database collection and API
