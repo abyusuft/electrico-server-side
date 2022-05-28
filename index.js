f82832f45b5e7671c352ff0c180bbd6b29e13aeb
abyusuft <imabuyusuf@gmail.com>
2022-05-28 15:40:43 +0600

    cors updated

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+    credentials: true,
+}));
 app.use(express.json());
 
 
